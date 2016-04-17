e0281627d5b5f799e161e6db124d5022a9ed762b
Jim Lawless <jimbo@radiks.net>
2016-04-17 17:01:16 -0500

    Added files via upload

---
+++ whereami.go
@@ -0,0 +1,36 @@
+// Copyright 2016 - by Jim Lawless
+// License: MIT / X11
+// See: http://www.mailsend-online.com/license2016.php
+//
+// This code may not conform to popular Go coding idioms
+
+package whereami
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+// return a string containing the file name, function name
+// and the line number of a specified entry on the call stack
+func WhereAmI(depthList ...int) string {
+	var depth int
+	if depthList == nil {
+		depth = 1
+	} else {
+		depth = depthList[0]
+	}
+	function, file, line, _ := runtime.Caller(depth)
+	return fmt.Sprintf("File: %s  Function: %s Line: %d", chopPath(file), runtime.FuncForPC(function).Name(), line)
+}
+
+// return the source filename after the last slash
+func chopPath(original string) string {
+	i := strings.LastIndex(original, "/")
+	if i == -1 {
+		return original
+	} else {
+		return original[i+1:]
+	}
+}
