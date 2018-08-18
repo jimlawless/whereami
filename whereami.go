4ec879ee5fb1c954e2f4e5a63791b5181af3fd07
Hendric Jabs <info@hendric-jabs.de>
2018-08-18 09:32:33 +0200

    Added function to reuse information. Editted existing function but without changing the output

--- whereami.go
+++ whereami.go
@@ -12,17 +12,57 @@ import (
 	"strings"
 )
 
+type WhereAmIDataOutput struct {
+	File       string
+	Function   string
+	LineNumber int
+}
+
+type WhereAmIDataInput struct {
+	DepthList          []int
+	FullFunctionOutput bool
+	FullFilePath       bool
+}
+
 // return a string containing the file name, function name
 // and the line number of a specified entry on the call stack
 func WhereAmI(depthList ...int) string {
+	data := WhereAmIData(WhereAmIDataInput{
+		DepthList:          depthList,
+		FullFilePath:       false,
+		FullFunctionOutput: true,
+	})
+	return fmt.Sprintf("File: %s  Function: %s Line: %d", data.File, data.Function, data.LineNumber)
+}
+
+// return  file name, function name and the line number
+// of a specified entry on the call stack
+func WhereAmIData(in WhereAmIDataInput) WhereAmIDataOutput {
 	var depth int
-	if depthList == nil {
+	if in.DepthList == nil {
 		depth = 1
 	} else {
-		depth = depthList[0]
+		depth = in.DepthList[0]
 	}
 	function, file, line, _ := runtime.Caller(depth)
-	return fmt.Sprintf("File: %s  Function: %s Line: %d", chopPath(file), runtime.FuncForPC(function).Name(), line)
+
+	out := WhereAmIDataOutput{}
+
+	if in.FullFunctionOutput {
+		out.Function = runtime.FuncForPC(function).Name()
+	} else {
+		out.Function = chopPath(runtime.FuncForPC(function).Name())
+	}
+
+	if in.FullFilePath {
+		out.File = file
+	} else {
+		out.File = chopPath(file)
+	}
+
+	out.LineNumber = line
+
+	return out
 }
 
 // return the source filename after the last slash
